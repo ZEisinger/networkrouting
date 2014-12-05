e3bfd57d24485922c320d2b9b438690232c96a6b
bdoyle <bdoyle@turnkey-technology.com>
2014-12-04 20:47:43 -0800

    File reading

--- Test.rb
+++ Test.rb
@@ -25,12 +25,40 @@ class Message
 end
 
 #TODO: Get command line args to determine what node I am
+
+node_name = ARGV[0]
+weights = ARGV[1]
+
 #TODO: Read file, figure out my IPs
+addrs = Array.new
+file = File.new("nodes-to-addrs.txt", "r")
+while (line = file.gets)
+	a = line.split(" ")
+	if (a[0] == node_name)
+		addrs.push(a[1])
+	end
+end
+file.close
+puts "My IPs"
+addrs.each { |a| puts a }
+
 #TODO: Read other file, figure out neighbors
+links = File.new(weights, "r")
+
+while (line = links.gets)
+	a = line.split(",")
+	if (addrs.include?(a[0]))
+		puts "My neighbor: #{a[1]} Weight: #{a[2]}"
+	end
+end
+links.close
+
 #TODO: Read other other file, figure out my weights to neighbors
 #TODO: Perform flood message algorithm
-#TODO: Once flooding is done, use Djikstras
-#TODO: Then we can 
+#TODO: Once flooding is done
+#TODO: Then we can start listening for command line messages 
+
+
 
 message = Message.new("CNTRL", "KILL ME PLEASE")
 puts message.build_message
